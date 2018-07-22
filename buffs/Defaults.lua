c582e6ce4fbb7a67a4efc3a2c8c8c83346d5c964
asuahsahua <asuahsahua@users.noreply.github.com>
2018-07-22 14:26:33 -0400

    Use LibPlayerSpells to get buffs for the current class, not just Rogue

--- buffs/Defaults.lua
+++ buffs/Defaults.lua
@@ -6,22 +6,28 @@ local bnot = _G.bit.bnot
 
 function RoguePowerBars:GetBarsetDefaults(useLibPlayerSpells)
     local buff = self.CreateBuffObject
+    -- local defaults = {
+    --     -- TODO: Fill these out with useful defaults
+    --     Buffs = {
+    --         -- Feint
+    --         buff(1966, 0.047, 0.675, 0.890),
+    --         -- Adrenaline Rush
+    --         buff(13750, 0.812, 0.639, 0.188),
+    --     },
+    --     Debuffs = {
+    --         -- Rupture
+    --         buff(1943)
+    --     },
+    --     OthersDebuffs = {
+    --         -- DH Chaos Brand
+    --         buff(255260)
+    --     }
+    -- }
+
     local defaults = {
-        -- TODO: Fill these out with useful defaults
-        Buffs = {
-            -- Feint
-            buff(1966, 0.047, 0.675, 0.890),
-            -- Adrenaline Rush
-            buff(13750, 0.812, 0.639, 0.188),
-        },
-        Debuffs = {
-            -- Rupture
-            buff(1943)
-        },
-        OthersDebuffs = {
-            -- DH Chaos Brand
-            buff(255260)
-        }
+        Buffs = {},
+        Debuffs = {},
+        OthersDebuffs = {}
     }
 
     -- TODO: Make this an option
@@ -44,10 +50,18 @@ function RoguePowerBars:_AddLibPlayerSpells(defaults)
     local c = LibPlayerSpells.constants
     local buff = self.CreateBuffObject
 
+    local classMap = {
+        "WARRIOR", "PALADIN", "HUNTER", "ROGUE", "PRIEST",
+        "DEATHKNIGHT", "SHAMAN", "MAGE", "WARLOCK", "MONK",
+        "DRUID", "DEMONHUNTER",
+    }
+
+    local _, _, classId = UnitClass("player")
+    local class = classMap[classId] or "ROGUE"
+
     -- Search LPS for our class' buffs and debuffs
-    local fmt = "Aura: %s %x"
     -- TODO: Search based on what class we are, not just rogue
-    for spellId, flags, providers, modifiedSpells, moreFlags in LibPlayerSpells:IterateSpells("ROGUE") do
+    for spellId, flags, providers, modifiedSpells, moreFlags in LibPlayerSpells:IterateSpells(class) do
         if band(flags, c.AURA) ~= 0 then
             if band(flags, c.PERSONAL) ~= 0 then
                 -- TODO: Check what happens when there are duplicate names
